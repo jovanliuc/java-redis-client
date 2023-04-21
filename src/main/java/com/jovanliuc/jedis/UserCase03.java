b41e934d4917650110e14db35b5d7172cc85bd91
Jovan L <327527689@qq.com>
2023-04-21 10:58:12 +0800

    Added the user case 03 for SSL case

---
+++ src/main/java/com/jovanliuc/jedis/UserCase03.java
@@ -0,0 +1,62 @@
+package com.jovanliuc.jedis;
+
+import org.testng.annotations.Test;
+import redis.clients.jedis.DefaultJedisClientConfig;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisClientConfig;
+import redis.clients.jedis.JedisPooled;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+
+public class UserCase03 {
+
+    @Test void test() throws GeneralSecurityException, IOException {
+        HostAndPort address = new HostAndPort("my-redis-instance.cloud.redislabs.com", 6379);
+
+        SSLSocketFactory sslFactory = createSslSocketFactory(
+                "./truststore.jks",
+                "secret!", // use the password you specified for keytool command
+                "./redis-user-keystore.p12",
+                "secret!" // use the password you specified for openssl command
+        );
+
+        JedisClientConfig config = DefaultJedisClientConfig.builder()
+                .ssl(true).sslSocketFactory(sslFactory)
+                .user("default") // use your Redis user. More info https://redis.io/docs/management/security/acl/
+                .password("secret!") // use your Redis password
+                .build();
+
+        JedisPooled jedis = new JedisPooled(address, config);
+        jedis.set("foo", "bar");
+        System.out.println(jedis.get("foo")); // prints bar
+    }
+
+    private SSLSocketFactory createSslSocketFactory(
+            String caCertPath, String caCertPassword, String userCertPath, String userCertPassword)
+            throws IOException, GeneralSecurityException {
+
+        KeyStore keyStore = KeyStore.getInstance("pkcs12");
+        keyStore.load(new FileInputStream(userCertPath), userCertPassword.toCharArray());
+
+        KeyStore trustStore = KeyStore.getInstance("jks");
+        trustStore.load(new FileInputStream(caCertPath), caCertPassword.toCharArray());
+
+        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance("X509");
+        trustManagerFactory.init(trustStore);
+
+        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("PKIX");
+        keyManagerFactory.init(keyStore, userCertPassword.toCharArray());
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
+
+        return sslContext.getSocketFactory();
+    }
+}
