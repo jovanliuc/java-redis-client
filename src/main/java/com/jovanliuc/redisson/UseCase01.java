69b601dae4de986350b9dff2eca6fbc19f757791
Jovan L <327527689@qq.com>
2023-04-22 22:25:59 +0800

    Added redisson dependencies and use case 01 for standlone usage

---
+++ src/main/java/com/jovanliuc/redisson/UseCase01.java
@@ -0,0 +1,41 @@
+package com.jovanliuc.redisson;
+
+import org.redisson.Redisson;
+import org.redisson.api.RMap;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.testng.annotations.Test;
+
+public class UseCase01 {
+
+    @Test
+    void test() {
+
+        // Create Redisson client configuration
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://localhost:6379")
+                .setDatabase(0);
+
+        // Create Redisson client instance
+        RedissonClient redisson = Redisson.create(config);
+
+        // Get a Redisson distributed map
+        RMap<String, String> map = redisson.getMap("myMap");
+
+        // Add some key-value pairs to the map
+        map.put("key1", "value1");
+        map.put("key2", "value2");
+        map.put("key3", "value3");
+
+        // Retrieve a value from the map by key
+        String value = map.get("key2");
+        System.out.println("Value for key2: " + value);
+
+        // Remove a key-value pair from the map
+        map.remove("key3");
+
+        // Shutdown the Redisson client
+        redisson.shutdown();
+    }
+}
